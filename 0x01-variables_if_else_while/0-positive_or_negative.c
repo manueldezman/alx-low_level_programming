3769db965cd645232bcbd2543416ffbc386b23b5
manueldezman <adelekeabdulganiy@gmail.com>
2022-04-08 08:16:00 -0700

    Polarity of a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.io>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ *  Return: Always(0)Sucess
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d", n);
+	if (n > 0)
+	{
+		printf("is Positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is Negative\n");
+	return (0);
+}
