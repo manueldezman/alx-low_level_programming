8102d42b3faabbea231a5ade097240f8a62f4e56
manueldezman <adelekeabdulganiy@gmail.com>
2022-04-08 02:33:52 -0700

    use puts to print output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * return - Always returns 0
- * 
+ * Return: Always 0(Success) 
+ *
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
