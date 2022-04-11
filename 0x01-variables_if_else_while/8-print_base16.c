c604a0811928409fdd912b8145e8b1f167986f96
manueldezman <adelekeabdulganiy@gmail.com>
2022-04-10 19:17:43 -0700

    print haxadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always(0)
+ *
+*/
+int main(void)
+{
+	int i = 48;
+	int j = 65;
+
+	while (i < 58)
+	{
+		putchar(i);
+	}
+	while (j < 71)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
