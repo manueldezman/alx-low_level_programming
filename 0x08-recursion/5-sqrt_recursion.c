5a781f49f65e9968a754897e467a13ccec193c77
manueldezman <adelekeabdulganiy@gmail.com>
2022-04-28 20:24:29 -0700

    initial commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+/**
+ * _sqrt - a function that returns
+ * the natural square root of a number.
+ * @n: The number
+ * @i: The initial guess
+ * Return: The square root of n
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - a function that returns
+ *the natural square root of a number.
+ * @n: The number
+ * Return: The square root or -1 if no square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
