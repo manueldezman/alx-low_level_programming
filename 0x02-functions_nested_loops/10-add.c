36943552f2e9e70568a1d45eb559e2bee94e124b
manueldezman <adelekeabdulganiy@gmail.com>
2023-02-22 05:47:16 +0100

    fixed some bugs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/** 
+*add - a function that adds two integersand returns the result
+*@num1: first number
+*@num2: second numberReturn: additiom of the two numbers
+*/ 
+ int add(int num1, int num2) 
+{
+		         
+return (num1 + num2); 
+}
