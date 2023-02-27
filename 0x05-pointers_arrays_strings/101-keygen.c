eca29e4a9612c78091fe3480c9859320322fd2e2
manueldezman <adelekeabdulganiy@gmail.com>
2023-02-27 22:19:14 +0100

    added advanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+*
+*/
+
+int main(void)
+{
+	int nuti
+	srand(time(0));
+
+	num = rand();
+	printf("Anything; %i\n", num);
+
+	return (0);
+}
