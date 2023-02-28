80d763dd4ab369521164e28c7e662606a414bb9f
manueldezman <adelekeabdulganiy@gmail.com>
2023-02-28 05:19:29 -0800

    fixed advanced task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,28 +6,28 @@
 * Return: integer. 
 */
 
-int _atoi(char *s)
+int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-	while (*(s + count) != '\0')
-	{		
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
-		if (*(s + count) == '-')
-			pn *= -1;
-		if ((*(s + count) >= '0') && (*(s + count) <= '9')
+		if (*(s + count) == '-')
+			pn *= -1;
+		if ((*(s + count) >= '0') && (*(s + count) <= '9')
 		{
-
-			if (size > 0)
-				m *= 10;
+			if (size > 0)
+				m *= 10;
 			size++;
 		}
-		count++;
+	count++;
 	}
- 	for (i = count - size; i < count; i++)
+
+	for (i = count - size; i < count; i++)
 	{
-		oi = oi + ((*(s + i) - 48) * m);
-		 m /= 10;
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
 	}
-	 return (oi * pn);
+	return (oi * pn);
 }
