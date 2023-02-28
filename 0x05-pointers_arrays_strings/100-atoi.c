5f0af861cadf88fdb6262c364b0949f23b4f2ad4
manueldezman <adelekeabdulganiy@gmail.com>
2023-02-28 05:22:39 -0800

    fixed advanced task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 			break;
 		if (*(s + count) == '-')
 			pn *= -1;
-		if ((*(s + count) >= '0') && (*(s + count) <= '9')
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
 				m *= 10;
