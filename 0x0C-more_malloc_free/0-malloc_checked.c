56ef89b1439d84ab9fe454935aebd6c76c34584e
manueldezman <adelekeabdulganiy@gmail.com>
2023-03-16 16:46:33 -0700

    problems involving memory reservation ussing malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that allocates memory
+ *
+ * @b: size of memory to be allocated
+ *
+ * Return: 98 for error or a pointer to the reserved memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *str;
+
+	str = malloc(b);
+
+	if (str == NULL)
+		exit(98);
+
+	return (str);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main"
+
+/**
+ * _calloc - a function that reserves memory for
+ * an array
+ *
+ * @nmemb: number of elements in the array
+ * @size: no of bytes of each elements
+ *
+ * Return: a pointer to the reserved memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, totalSize = nmemb * size;
+	char *array;
+
+	if (totalSize == 0)
+		return (NULL);
+
+	array = malloc(totalSize);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
+	return (array);
+}
